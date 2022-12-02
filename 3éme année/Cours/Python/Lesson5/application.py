113031482d5b7779477b1e6137f935fbd41d04eb
Yoshi <naghazakihorochi@gmail.com>
2022-12-02 17:17:06 +0100

    Added application.py in 3éme année\Cours\Python\Lesson5

---
+++ 3éme année/Cours/Python/Lesson5/application.py
@@ -0,0 +1,68 @@
+from numpy import *
+
+E = dict(ch = "", chy = "")
+TabE = array([E] * 15)
+Tab = array([str] * 15)
+Mat = array([[str] * 15] * 15)
+
+def saisir(a, b):
+    n = int(input("donner n: "))
+    while n < a or n > b:
+        n = int(input("donner n: "))
+    return n
+
+def remplir(Tab, n):
+    for i in range(n):
+        Tab[i] = str(input("donner une chaine: "))
+        while len(Tab[i]) < 3:
+            Tab[i] = str(input("donner une chaine: "))
+
+def affichage(TabE, n):
+    print("")  # pour que l'affichage fasse un espace entre les données d'utilisateur et les données d'affichage
+    for i in range(n):
+        print(TabE[i]["ch"])
+        print(TabE[i]["chy"])
+        print("-----------")  # pour que l'affichage fasse un espace entre les différentes données d'enregistrement
+
+def construit(TabE, Tab, n):
+    for i in range(n):
+        ch = espace(Tab[i])
+        chy = cryptage(ch)
+        TabE[i] = {}
+        TabE[i]["ch"] = Tab[i]
+        TabE[i]["chy"] = chy
+
+def espace(ch):
+    while len(ch) % 3 != 0:
+        ch += " "
+    return ch
+
+def cryptage(ch):
+    k = 0
+    for i in range(len(ch) // 3):
+        for j in range(3):
+            Mat[i, j] = ch[k]
+            k += 1
+    for x in range(3):
+        Tri(Mat, len(ch) // 3, x)
+    ch1 = ""
+    for i in range(len(ch) // 3):
+        for j in range(3):
+            ch1 += Mat[i, j]
+    return ch1
+
+def Tri(Mat, n, x):
+    permut = True
+    while permut:
+        permut = False
+        for i in range(n - 1):
+            if Mat[i, x] > Mat[i + 1, x]:
+                temp = Mat[i, x]
+                Mat[i, x] = Mat[i + 1, x]
+                Mat[i + 1, x] = temp
+                permut = True
+
+n = saisir(3, 15)
+remplir(Tab, n)
+construit(TabE, Tab, n)
+affichage(TabE, n)
